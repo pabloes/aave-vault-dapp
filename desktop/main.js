d1719031dc3ed7aabfc972bda1994dfce018a2d4
pabloes <estornut@gmail.com>
2025-08-29 13:43:29 +0200

    desktop: add Electron app scaffold (main, index, builder config)

---
+++ desktop/main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow, shell } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      contextIsolation: true,
+    }
+  })
+  win.removeMenu()
+  win.loadFile(path.join(__dirname, 'index.html'))
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url)
+    return { action: 'deny' }
+  })
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
